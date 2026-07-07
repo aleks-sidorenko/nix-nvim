4b39574fa04bbfea82df91621e4c5e2fa564aa7e
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2026-07-07 13:39:49 +0300

    feat: flake-parts flake exposing packages.default

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A configurable, batteries-included Neovim distribution built with nixvim.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixvim = {
+      url = "github:nix-community/nixvim/nixos-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{ flake-parts, nixvim, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+
+      perSystem =
+        { pkgs, system, ... }:
+        {
+          packages.default = nixvim.legacyPackages.${system}.makeNixvimWithModule {
+            inherit pkgs;
+            module = ./nvim;
+          };
+        };
+    };
+}
